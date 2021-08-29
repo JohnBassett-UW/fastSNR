8a81c9cd0e84f2ae74b4c7110c14e1d0a68d9961
John Bassett <jbassett@fredhutch.org>
2021-08-29 11:02:45 -0700

    included global variables to track num comparisons.

--- fastSNR.c
+++ fastSNR.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
+
+int comparisons = 0;
+double minComparisons;
+int maxComparisons = 0;
+double totalComparisons = 0;
 
 void swap(int* x, int* y);
 void heap(int vec[], int n, int idx);
 void buildNodes(int vec[], int n);
 void printh(int vec[], int n);
 double signalNoise(int vec[]);
+double log10(double x);
 
 int main() {
-    const int n = 10; 
-    const int ntests = 1000; // number of tests to conduct
+    const int n = 1000;
+    const int ntests = 10000; // number of tests to conduct
     int success = 0; // increments if test succeeds
     int x; // psuedorandom index of array
     int y; // psuedorandom index of array
@@ -23,18 +30,18 @@ int main() {
     bool heaptest = false;
     bool snrtest = false;
 
-    
-		
+    minComparisons = n*log10((double)n)/log10(2.0) + n;
     int *array = (int *) calloc(n, sizeof(int));
     if(array == NULL){
 		printf("calloc size %d failed!\n", n);
 		exit(1);
 	}
+  
     //iterate through 1000 rounds of testing
     for(int t = 0; t <ntests; t++){
     //generate array
     	srand(t + time(NULL)); 
-    	for( int i = 0; i < 10; i++){
+    	for( int i = 0; i < n; i++){
 		array[i] = rand() % 1000 + 1;
     	}
    
@@ -52,11 +59,12 @@ int main() {
 
    	//printf("\n");
    //build heap
-    	printh(array, n);
+    	//printh(array, n);
+	comparisons  = 0;
 	buildNodes(array, n);
-	printf(" <-> ");
-	printh(array, n);
-	printf("\n");
+	//printf(" <-> ");
+	//printh(array, n);
+	//printf(" comparisons made: %d \n", comparisons);
    //test heap
 	for(int h = 3; h < n; h++){
 		if(array[0] < array[h] || array[1] < array[h] || array[2] < array[h]){
@@ -91,10 +99,20 @@ int main() {
 	if(snrtest == true && heaptest == true && swaptest == true){
 		success += 1;
 	}
+	
+	if(comparisons > maxComparisons){
+		maxComparisons = comparisons;
+	}
+	if(comparisons < minComparisons){
+		minComparisons = comparisons;
+	}
 
+	totalComparisons += comparisons;
 	//printf("snr: %d, heap: %d, swap: %d \n", snrtest, heaptest, swaptest);
     } 
     printf("fastSNR succeeded %d / %d times \n", success, ntests);
+    printf("Avg Comparisons: %f  \n", totalComparisons/ntests);
+    printf("max Comparisons: %d   min comparisons: %f \n", maxComparisons, trunc(minComparisons));
     free(array);
     return 0;
     
@@ -103,7 +121,7 @@ int main() {
 void swap(int *x, int *y)
 {
 	int tmp = *x;
-	//printf("%ls, %ls, %ls", x, y, tmp);
+	//printf("%p, %p, %p", x, y, tmp);
 	*x = *y;
 	*y = tmp;
 }
@@ -124,15 +142,15 @@ void heap(int vec[], int n, int idx)
 	int high = idx;
 	int left = 2 * idx + 1;
 	int right = 2 * idx + 2;
-
+	comparisons ++;
 	if( left < n && vec[left] > vec[high]){
 		high = left;
 	}
-	 
+	comparisons ++;
 	if( right < n && vec[right] > vec[high]){
 		high = right;
 	}
-
+	comparisons ++;
 	if (high != idx){
 		swap(&vec[idx], &vec[high]);
 //		printf("%d <-> %d    ", vec[idx], vec[high]);
@@ -145,6 +163,7 @@ void printh(int vec[], int n){
 	}
 }
 double signalNoise(int vec[]){
+	comparisons ++;
 	if(vec[2] >= vec[1]){
 	return((double)vec[0]/vec[2]);
 	}
